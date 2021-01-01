db906cf4effabd95052fdf6cabfd5769900224e0
Lars Stegman <larsstegman@outlook.com>
2021-01-01 14:09:33 +0100

    Initial Commit

---
+++ Shared/EffectScapadesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectScapadesApp.swift
+//  Shared
+//
+//  Created by Lars Stegman on 01/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct EffectScapadesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
